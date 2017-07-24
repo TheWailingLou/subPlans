1c468e17b9fddf44c12a625cc50fda601ec97025
Louis Antweiler <Louis.Antweiler@Gmail.com>
2017-07-24 02:13:51 -0600

    start an HTML mockup

---
+++ htmlMockup/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.dropdownButton').click(function(){
+    $('.mainDropdown').toggle()
+  })
+  $('.logInButton').click(function(){
+    alert('Auth 0 Asks you to log in with google now.')
+  })
+})
